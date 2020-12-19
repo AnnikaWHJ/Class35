8b15341cac13b30d1874cae92b96e0f925935c99
AnnikaWHJ <mishr10333@gapps.uwcsea.edu.sg>
2020-12-19 11:15:39 +0800

    Today we created a multiplayer game

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 var ball;
+var database,position
 
 function setup(){
+   database = firebase.database
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var hPosition = firebase.database.ref('ball/position')
+    hPosition.on("value",readPosition,showError)
+
 }
 
 function draw(){
@@ -23,7 +28,18 @@ function draw(){
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function readPosition(data){
+position = data.val()
+ball.x = position.x
+ball.y = position.y
+
+
+
+}
+
+function writePosition(x,y){
+   database.ref('ball/position').set({
+       'x': position.x + x,
+       'y': position.y + y,
+   })
 }
